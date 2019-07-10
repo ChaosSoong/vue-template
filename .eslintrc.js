34b2c4b6045201fac9a67cbbbf838daad219ce7f
songweichao <songweichao@czb365.com>
2019-07-10 19:31:17 +0800

    feat:添加配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,20 @@ module.exports = {
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parserOptions: {
-    parser: "babel-eslint"
-  }
+    parser: "babel-eslint",
+    // ECMAScript 模块
+    sourceType: 'module',
+    // 想使用的额外的语言特性:
+    ecmaFeatures: {
+      // 允许在全局作用域下使用 return 语句
+      globalReturn: true,
+      // impliedStric
+      impliedStrict: true,
+      // 启用 JSX
+      jsx: true
+    }
+  },
+  plugins: [
+    'vue'
+  ]
 };
